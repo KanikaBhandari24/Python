a109e0620d656f4b3c5ae5cf70ffaabd9bddfe87
Kanika Bhandari <145848814+KanikaBhandari24@users.noreply.github.com>
2024-07-02 10:40:45 +0530

    Delete Project1 directory

--- Project1/main (1).py
+++
@@ -1,9 +0,0 @@
-print("You are Welcomed to the Band Name Generator project!")
-
-#print("Click Run to run the final project you will build.")
-
-city=input("What's the name of the city you grew up in?\n")
-
-pet=input("What's your pet's name?\n")
-
-print("Your band name could be " + city + " " + pet)
