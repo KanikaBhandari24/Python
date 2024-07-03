8bd13fb4be177a93f9fcaf3718fa28b8c426c1a7
Kanika Bhandari <145848814+KanikaBhandari24@users.noreply.github.com>
2024-07-03 20:10:41 +0530

    Add files via upload

---
+++ Coding/lists.py
@@ -0,0 +1,12 @@
+# list=["Delhi", "Mumbai", "UK", "Banglore"]
+# print(list[2])
+# list[1]="Maharashtra"
+# print(list)
+# list.append("Chennai")
+# print(list)
+
+#NESTED-LIST 
+fruits=['Orange','Mango','Pineapple','Apple']
+vegetables=['Carrot','Brinjal','Tomato','Potato']
+eatables=[fruits,vegetables]
+print(eatables)
---
+++ Coding/random module.py
@@ -0,0 +1,9 @@
+import random
+
+#for interger 
+# rand=random.randint(2, 28)
+# print(rand)
+
+#for float
+rand=random.random()
+print(rand)
