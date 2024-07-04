e57b27821e0ea4eada4232c8a7cac47e192fe640
Kanika Bhandari <145848814+KanikaBhandari24@users.noreply.github.com>
2024-07-04 20:58:53 +0530

    Add files via upload

---
+++ Coding/for loop.py
@@ -0,0 +1,17 @@
+fruits=["orange", "peach", "mango"]
+for fruit in fruits:
+  print(fruit)
+  print(fruit + " pie")
+  print(fruits)
+print(fruits) #OUTSIDE FOR LOOP
+
+for n in range(1, 10):
+  print(n)
+
+for a in range(1, 20, 3): # 3 will be the gap b/w numbers
+  print(a)
+
+total=0
+for b in range(1, 101):
+  total += b
+  print(total)
