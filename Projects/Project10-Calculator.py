21f9cf1aaf9386a1fe355954b4b70321fd6d264b
Kanika Bhandari <145848814+KanikaBhandari24@users.noreply.github.com>
2024-07-08 10:02:05 +0530

    Add files via upload

---
+++ Projects/Project10-Calculator.py
@@ -0,0 +1,50 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {"+" : add, "-" : sub, "*" : multiply, "/" : divide}
+
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    #num2 = int(input("Enter the second number: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        op = input("Which operation do you want to perform?\n")
+        num2 = float(input("Enter the next number: "))
+        calculation = operations[op]
+        answer = calculation(num1, num2)
+        print(f"{num1} {op} {num2} = {answer}")
+
+        if input(f"Type 'Y' to continue with {answer} or type 'N' to start a new calculation\n") == "Y":
+            num1 = answer
+        else:
+            should_continue = False
+            #some condition to be met
+            calculator()   #RECURSION
+calculator()
